cea908174514d73922b84c93cf854218cd4aa158
Nikita Golubev <39840774+TheFunnyDay@users.noreply.github.com>
2020-07-26 20:40:35 +0500

    Update openinfo.js

--- openinfo.js
+++ openinfo.js
@@ -1,7 +1,9 @@
 const openinfo = [{
     launcherVer = "beta 0.1",
     launcherAuthor = "TheFunnyDay",
-    launcherName = "JSR Launcher"
+    launcherName = "JSR Launcher",
+    bgNow = "https://i.ibb.co/NpBsGrz/background.jpg",
+    charNow = "https://i.ibb.co/FXJJJ6J/Ephnel-Character-Art.png",
 }];
 
 document.getElementById("jsrlauncher-ver").innerHTML = `
