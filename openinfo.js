f03ecf2deafba958d3ab7ca4f0b98e3d1faac14c
Nikita Golubev <39840774+TheFunnyDay@users.noreply.github.com>
2020-07-26 20:33:58 +0500

    Create openinfo.js

---
+++ openinfo.js
@@ -0,0 +1,15 @@
+const openinfo = [{
+    launcherVer = "beta 0.1",
+    launcherAuthor = "TheFunnyDay",
+    launcherName = "JSR Launcher"
+}];
+
+document.getElementById("jsrlauncher-ver").innerHTML = `
+ ${openinfo.launcherVer}
+`;
+document.getElementById("jsrlauncher-author").innerHTML = `
+ ${openinfo.launcherAuthor}
+`;
+document.getElementById("jsrlauncher-name").innerHTML = `
+ ${openinfo.launcherName}
+`;
